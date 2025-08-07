ee3d9a910dfa8e446bf5a8a09a95e561e1cb84d6
Kian9apc <kplorica@student.apc.edu.ph>
2025-08-07 03:06:56 +0000

    initial commit

---
+++ Week 03/TwoInputs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Age: ");
+        int age = input.nextInt();
+
+        System.out.print("Enter Favourite Number: ");
+        int favouriteNumber = input.nextInt();
+
+        System.out.println("Your age is: " + age);
+        System.out.println("Your favourite number is: " + favouriteNumber);
+        
+        System.out.println("When added together: " + (age + favouriteNumber));
+        System.out.println("When subtracted together: " + (age - favouriteNumber));
+        System.out.println("When multiplied together: " + (age * favouriteNumber));
+        System.out.println("Di ko alam pano sa division huhu");
+    }
+}
