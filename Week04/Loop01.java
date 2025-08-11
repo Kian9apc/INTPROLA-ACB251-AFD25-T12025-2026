7de6dd3abcd430908ce8b4a4ff56de893b422632
Kian9apc <kplorica@student.apc.edu.ph>
2025-08-11 03:06:49 +0000

    coded

--- Week04/Loop01.java
+++ Week04/Loop01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class LoopExercise {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
     System.out.println("Enter first number");
