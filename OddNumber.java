d32cdaa3250ef33c902bafe6a0c58aacb9c559dc
Kian9apc <kplorica@student.apc.edu.ph>
2025-08-14 01:53:15 +0000

    oddnumbers

---
+++ OddNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OddNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int start = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int end = input.nextInt();
+
+        System.out.println("Even numbers from " + start + " to " + end + ":");
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
