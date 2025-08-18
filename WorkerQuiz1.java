65864c5f0d27365b22b7effc9b4b70939dc29a2d
Kian9apc <kplorica@student.apc.edu.ph>
2025-08-18 02:16:23 +0000

    new quiz

--- WorkerQuiz1.java
+++ WorkerQuiz1.java
@@ -1,22 +1,20 @@
 public class WorkerQuiz1 {
-    
+
     public void processRange(int start, int end) {
         int i = start;
 
+        // While loop: check odd and prime
         while (i <= end) {
-            if (i % 2 != 0) { // odd
-                if (isPrime(i)) {
-                    System.out.println(i + " is odd and a prime number.");
-                }
+            if (i % 2 != 0 && isPrime(i)) {
+                System.out.println(i + " is odd and a prime number.");
             }
             i++;
         }
 
+        // For loop: check even and divisible by 3
         for (int j = start; j <= end; j++) {
-            if (j % 2 == 0) { // even
-                if (j % 3 == 0) {
-                    System.out.println(j + " is even and divisible by 3.");
-                }
+            if (j % 2 == 0 && j % 3 == 0) {
+                System.out.println(j + " is even and divisible by 3.");
             }
         }
     }
